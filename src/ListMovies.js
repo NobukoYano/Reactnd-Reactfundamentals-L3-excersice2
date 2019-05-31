863ff79741124dd34729ea37c3b9e1f6cd1a887b
Nobuko Yano <nobukoyano31@gmail.com>
2019-05-31 11:10:32 +0000

    initial

---
+++ src/ListMovies.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ListMovies extends React.Component{
+	const profiles = this.props.profiles;
+	const users = this.props.users;
+	const movies = this.props.movies;
+
+  render(){
+  
+  
+  }
+
+}
